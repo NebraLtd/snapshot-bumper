3c5dd64458ca08753913b1e49d2b0f2ebd221516
Ryan Walmsley <ryan@ryanteck.uk>
2021-05-08 14:55:06 +0100

    Bumps bumper program to latest version.

--- cacher.py
+++ cacher.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 # Program that stores blocks to database.
 
 from time import sleep
@@ -8,7 +9,7 @@ from urllib.error import URLError, HTTPError
 from pprint import pprint
 
 import sqlite3
-con = sqlite3.connect('block_cache.db')
+con = sqlite3.connect('/opt/nebrascripts/snapshot-bumper/block_cache.db')
 cur = con.cursor()
 # db = TinyDB('block_cache.json')
 
@@ -20,43 +21,50 @@ apiUrl = "https://api.helium.io/v1/blocks/"
 
 # Get Blockchain Height
 
-try:
-    heightUrl = apiUrl +  "height"
-    with urllib.request.urlopen(heightUrl) as response:
-        heightJson = json.loads(response.read())
-        blockchainHeight = (heightJson['data']['height'])
-except HTTPError as e:
-    #Most likely not generate yet. Sleep
-    print("Waiting for request")
 
-    sleep(2)
 
 print(blockchainHeight)
 
 # Getting latest entry
 
-try:
-    cur.execute('SELECT * FROM blocks ORDER BY height DESC')
 
-    lastBlock = cur.fetchone()[4]
-except TypeError as e:
-    pass
 
-print("Loading blocks into cache db")
 
-while(lastBlock < blockchainHeight):
-    blockUrl = apiUrl + str(lastBlock)
+while True:
+    print("Loading blocks into cache db")
+
     try:
-        with urllib.request.urlopen(blockUrl) as response:
-            blockJson = json.loads(response.read())['data']
-            #pprint(blockJson)
-            cur.execute("insert into blocks values (?, ?, ?, ?, ?, ?)", (blockJson['transaction_count'], blockJson['time'], blockJson['snapshot_hash'], blockJson['prev_hash'], blockJson['height'], blockJson['hash']))
-            con.commit()
+        heightUrl = apiUrl +  "height"
+        with urllib.request.urlopen(heightUrl) as response:
+            heightJson = json.loads(response.read())
+            blockchainHeight = (heightJson['data']['height'])
     except HTTPError as e:
         #Most likely not generate yet. Sleep
         print("Waiting for request")
+
         sleep(2)
-    except sqlite3.IntegrityError as e:
-        print("Already in DB")
-    print(lastBlock)
-    lastBlock+=1
+
+    try:
+        cur.execute('SELECT * FROM blocks ORDER BY height DESC')
+
+        lastBlock = cur.fetchone()[4]
+    except TypeError as e:
+        pass
+
+    while(lastBlock < blockchainHeight):
+        blockUrl = apiUrl + str(lastBlock)
+        try:
+            with urllib.request.urlopen(blockUrl) as response:
+                blockJson = json.loads(response.read())['data']
+                #pprint(blockJson)
+                cur.execute("insert into blocks values (?, ?, ?, ?, ?, ?)", (blockJson['transaction_count'], blockJson['time'], blockJson['snapshot_hash'], blockJson['prev_hash'], blockJson['height'], blockJson['hash']))
+                con.commit()
+        except HTTPError as e:
+            #Most likely not generate yet. Sleep
+            print("Waiting for request")
+            sleep(2)
+        except sqlite3.IntegrityError as e:
+            print("Already in DB")
+        print(lastBlock)
+        lastBlock+=1
+    sleep(60)
---
+++ readCache.py
@@ -0,0 +1,53 @@
+import sqlite3
+import datetime
+import urllib.request
+import json
+import base64
+from urllib.error import URLError, HTTPError
+con = sqlite3.connect('block_cache.db')
+cur = con.cursor()
+
+blockchainHeight = 0
+
+apiUrl = "https://api.helium.io/v1/blocks/"
+
+# Get Blockchain Height
+
+try:
+    heightUrl = apiUrl +  "height"
+    with urllib.request.urlopen(heightUrl) as response:
+        heightJson = json.loads(response.read())
+        blockchainHeight = (heightJson['data']['height'])
+except HTTPError as e:
+    # Most likely not generate yet. Sleep
+    print("Waiting for request")
+
+    sleep(2)
+
+try:
+    cur.execute('SELECT * FROM blocks WHERE snapshot_hash IS NOT "" ORDER BY height DESC')
+
+    lastBlock = cur.fetchone()
+except TypeError as e:
+    pass
+
+# print(lastBlock)
+date = datetime.datetime.fromtimestamp(lastBlock[1]).strftime("%m/%d/%Y, %H:%M:%S")
+print("Last Blessed Block Date: " + date)
+print(str(blockchainHeight-lastBlock[4]) + " Blocks left to sync")
+snapshot_hash = lastBlock[2]
+snapshot_hash += "=" * ((3- len(snapshot_hash) % 3) % 3)
+hashArray = list(base64.urlsafe_b64decode(snapshot_hash))
+# print(hashArray)
+hashString = ','.join(str(e) for e in hashArray)
+#print(hashString)
+with open('baseConfig.config', 'r') as file :
+  filedata = file.read()
+
+# Replace the target string
+filedata = filedata.replace('bsbheight', str(lastBlock[4]))
+filedata = filedata.replace('bsbhash', str(hashString))
+
+# Write the file out again
+with open('docker.config', 'w') as file:
+  file.write(filedata)
